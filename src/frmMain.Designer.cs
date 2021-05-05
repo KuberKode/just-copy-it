2a00989c4e29c29f5984ebc1a53233155b680be8
Johan Strydom <fyrewolfe@gmail.com>
2021-05-05 14:32:21 +0200

    v0.1 Commit
    - Add initial, working as designed v0.1

---
+++ src/frmMain.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace JustCopyIT
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtSourceFls = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnDestinationFolder = new System.Windows.Forms.Button();
+            this.txtDestinationFolder = new System.Windows.Forms.TextBox();
+            this.chkKeepFolderStruct = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRootFolder = new System.Windows.Forms.Button();
+            this.txtRootFolder = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFilesDialog = new System.Windows.Forms.OpenFileDialog();
+            this.foldersBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.cbOverWrite = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Source File(s)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Just Copy IT";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.btnDestinationFolder);
+            this.groupBox1.Controls.Add(this.txtSourceFls);
+            this.groupBox1.Controls.Add(this.txtDestinationFolder);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(18, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(488, 273);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "File(s) Location(s)";
+            // 
+            // txtSourceFls
+            // 
+            this.txtSourceFls.Location = new System.Drawing.Point(22, 43);
+            this.txtSourceFls.Multiline = true;
+            this.txtSourceFls.Name = "txtSourceFls";
+            this.txtSourceFls.Size = new System.Drawing.Size(387, 162);
+            this.txtSourceFls.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(415, 42);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(58, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(415, 71);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(58, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbOverWrite);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.btnRootFolder);
+            this.groupBox2.Controls.Add(this.txtRootFolder);
+            this.groupBox2.Controls.Add(this.chkKeepFolderStruct);
+            this.groupBox2.Location = new System.Drawing.Point(18, 338);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(488, 100);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Copy Options";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Destination Folder";
+            // 
+            // btnDestinationFolder
+            // 
+            this.btnDestinationFolder.Location = new System.Drawing.Point(415, 229);
+            this.btnDestinationFolder.Name = "btnDestinationFolder";
+            this.btnDestinationFolder.Size = new System.Drawing.Size(38, 28);
+            this.btnDestinationFolder.TabIndex = 9;
+            this.btnDestinationFolder.Text = "...";
+            this.btnDestinationFolder.UseVisualStyleBackColor = true;
+            this.btnDestinationFolder.Click += new System.EventHandler(this.btnDestinationFolder_Click);
+            // 
+            // txtDestinationFolder
+            // 
+            this.txtDestinationFolder.Location = new System.Drawing.Point(22, 234);
+            this.txtDestinationFolder.Name = "txtDestinationFolder";
+            this.txtDestinationFolder.Size = new System.Drawing.Size(387, 20);
+            this.txtDestinationFolder.TabIndex = 8;
+            this.txtDestinationFolder.Text = "C:\\Temp\\Out";
+            // 
+            // chkKeepFolderStruct
+            // 
+            this.chkKeepFolderStruct.AutoSize = true;
+            this.chkKeepFolderStruct.Location = new System.Drawing.Point(22, 22);
+            this.chkKeepFolderStruct.Name = "chkKeepFolderStruct";
+            this.chkKeepFolderStruct.Size = new System.Drawing.Size(166, 17);
+            this.chkKeepFolderStruct.TabIndex = 10;
+            this.chkKeepFolderStruct.Text = "Keep Source Folder Structure";
+            this.chkKeepFolderStruct.UseVisualStyleBackColor = true;
+            this.chkKeepFolderStruct.CheckedChanged += new System.EventHandler(this.chkKeepFolderStruct_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Root Folder";
+            // 
+            // btnRootFolder
+            // 
+            this.btnRootFolder.Enabled = false;
+            this.btnRootFolder.Location = new System.Drawing.Point(415, 58);
+            this.btnRootFolder.Name = "btnRootFolder";
+            this.btnRootFolder.Size = new System.Drawing.Size(38, 28);
+            this.btnRootFolder.TabIndex = 13;
+            this.btnRootFolder.Text = "...";
+            this.btnRootFolder.UseVisualStyleBackColor = true;
+            this.btnRootFolder.Click += new System.EventHandler(this.btnRootFolder_Click);
+            // 
+            // txtRootFolder
+            // 
+            this.txtRootFolder.Enabled = false;
+            this.txtRootFolder.Location = new System.Drawing.Point(22, 63);
+            this.txtRootFolder.Name = "txtRootFolder";
+            this.txtRootFolder.Size = new System.Drawing.Size(387, 20);
+            this.txtRootFolder.TabIndex = 12;
+            this.txtRootFolder.Text = "C:\\Temp\\In";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(397, 454);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 30);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Just Do it!";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFilesDialog
+            // 
+            this.openFilesDialog.Filter = "AllFiles|*.*";
+            this.openFilesDialog.InitialDirectory = "C:\\Temp\\In";
+            this.openFilesDialog.Multiselect = true;
+            // 
+            // foldersBrowserDialog
+            // 
+            this.foldersBrowserDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // cbOverWrite
+            // 
+            this.cbOverWrite.AutoSize = true;
+            this.cbOverWrite.Location = new System.Drawing.Point(233, 22);
+            this.cbOverWrite.Name = "cbOverWrite";
+            this.cbOverWrite.Size = new System.Drawing.Size(130, 17);
+            this.cbOverWrite.TabIndex = 14;
+            this.cbOverWrite.Text = "Overwrite existing files";
+            this.cbOverWrite.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 505);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Just Copy IT v0.1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtSourceFls;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnDestinationFolder;
+        private System.Windows.Forms.TextBox txtDestinationFolder;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRootFolder;
+        private System.Windows.Forms.TextBox txtRootFolder;
+        private System.Windows.Forms.CheckBox chkKeepFolderStruct;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFilesDialog;
+        private System.Windows.Forms.FolderBrowserDialog foldersBrowserDialog;
+        private System.Windows.Forms.CheckBox cbOverWrite;
+    }
+}
+
---
+++ src/frmMain.cs
@@ -0,0 +1,168 @@
+﻿/*
+MIT License
+
+Copyright (c) 2021 Johan Strydom
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace JustCopyIT
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtSourceFls.Text = "";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            DialogResult dRes = openFilesDialog.ShowDialog(this);
+            if (dRes == DialogResult.OK)
+            {
+                foreach(string fl in openFilesDialog.FileNames) {
+                    if (txtSourceFls.TextLength > 0) {
+                        txtSourceFls.Text += Environment.NewLine;
+                    }
+                    txtSourceFls.Text += fl;
+                }
+                
+            }
+        }
+
+        private void btnDestinationFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult dRes = foldersBrowserDialog.ShowDialog(this);
+            if (dRes == DialogResult.OK)
+            {
+                txtDestinationFolder.Text = foldersBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void chkKeepFolderStruct_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (chkKeepFolderStruct.Checked)
+            {
+                txtRootFolder.Enabled = true;
+                btnRootFolder.Enabled = true;
+            }
+            if (!chkKeepFolderStruct.Checked)
+            {
+                txtRootFolder.Enabled = false;
+                btnRootFolder.Enabled = false;
+            }
+        }
+
+        private void btnRootFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult dRes = foldersBrowserDialog.ShowDialog(this);
+            if (dRes == DialogResult.OK)
+            {
+                txtRootFolder.Text = foldersBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+
+            if (txtSourceFls.Text.Length == 0)
+            {
+                MessageBox.Show(this, "Please select source file(s)");
+                this.Cursor = Cursors.Default;
+                return;
+            }
+
+            if (txtDestinationFolder.Text.Length == 0)
+            {
+                MessageBox.Show(this, "Please select a destination folder");
+                this.Cursor = Cursors.Default;
+                return;
+            }
+
+            char DS = Path.DirectorySeparatorChar;
+            string targetLocation = txtDestinationFolder.Text;
+
+            foreach (string sourceFileLine in txtSourceFls.Lines)
+            {
+                try {
+
+                    string[] sourcePath = sourceFileLine.Split('\\');
+                    string sourceFile = sourcePath.Last();
+
+                    
+                    //keep folder structure according to root
+                    if (chkKeepFolderStruct.Checked) {
+                        if (!sourceFileLine.Contains(txtRootFolder.Text)) {
+                            MessageBox.Show(this, "Error: Root (" + txtRootFolder.Text + ") not found for file " + sourceFileLine);
+                            continue;
+                        }
+                        string rootFolder = txtRootFolder.Text;
+                        if (rootFolder.Last() == DS) {
+                            rootFolder = rootFolder.Substring(0, rootFolder.Length - 1);
+                        }
+                        sourceFile = sourceFileLine.Replace(rootFolder + DS,"");
+                        sourcePath = sourceFile.Split('\\');
+                        //See if directories exist then create it if not found
+                        string subStruct = targetLocation;
+                        for (int i = 0; i< (sourcePath.Length-1); i++) {
+                            subStruct += DS + sourcePath[i];
+                            if (!System.IO.Directory.Exists(subStruct)) {
+                                System.IO.Directory.CreateDirectory(subStruct);
+                            }
+                        }
+                    }
+                    string destFileLine = targetLocation + DS + sourceFile;
+
+                    bool overwrite = false;
+                    if (cbOverWrite.Checked) {
+                        overwrite = true;
+                    }
+
+                    System.IO.File.Copy(sourceFileLine, destFileLine, overwrite);
+
+                }
+                catch (System.Exception error)
+                {
+                    MessageBox.Show(this, "Error: " + error.Message);
+                }
+            }
+            
+            this.Cursor = Cursors.Default;
+            MessageBox.Show(this, "~ Copy Finished ~");
+            btnClear_Click(sender, e);
+        }
+    }
+}
